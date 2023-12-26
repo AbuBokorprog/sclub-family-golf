e23344d8c0ba4e93a9e8aeadae49c69af226a0c4
AbuBokorprog <abubokor1066@gmail.com>
2023-12-26 19:54:05 +0600

    Page 2

--- JS/script.js
+++ JS/script.js
@@ -1,13 +1,13 @@
 const tl = gsap.timeline();
 const crs = document.querySelector("#cursor");
 const crsBlur = document.querySelector("#cursor-blur");
+
 document.addEventListener("mousemove", function (dets) {
   crs.style.left = dets.x + "px";
   crs.style.top = dets.y + "px";
-  crsBlur.style.left = dets.x - 150 + "px";
-  crsBlur.style.top = dets.y - 150 + "px";
+  crsBlur.style.left = dets.x - 250 + "px";
+  crsBlur.style.top = dets.y - 270 + "px";
 });
-document.addEventListener("mousemove", function (dets) {});
 
 tl.to("#nav", {
   backgroundColor: "#000",
@@ -16,7 +16,6 @@ tl.to("#nav", {
   scrollTrigger: {
     trigger: "#nav",
     scroller: "body",
-    // markers: true,
     start: "top -10%",
     end: "top -11%",
     scrub: 2,
@@ -28,7 +27,6 @@ tl.to("#main", {
   scrollTrigger: {
     trigger: "#main",
     scroller: "body",
-    markers: true,
     start: "top -30%",
     end: "top -80%",
     scrub: 2,
