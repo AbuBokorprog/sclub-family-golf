ad003ba8ef8256e378c84da3af2566fc61c6f275
AbuBokorprog <abubokor1066@gmail.com>
2023-12-28 00:13:41 +0600

    Locomotive scroll implement

--- JS/script.js
+++ JS/script.js
@@ -1,3 +1,45 @@
+function locomotive() {
+  gsap.registerPlugin(ScrollTrigger);
+
+  // Using Locomotive Scroll from Locomotive https://github.com/locomotivemtl/locomotive-scroll
+
+  const locoScroll = new LocomotiveScroll({
+    el: document.querySelector("#main"),
+    smooth: true,
+  });
+  // each time Locomotive Scroll updates, tell ScrollTrigger to update too (sync positioning)
+  locoScroll.on("scroll", ScrollTrigger.update);
+
+  // tell ScrollTrigger to use these proxy methods for the "#main" element since Locomotive Scroll is hijacking things
+  ScrollTrigger.scrollerProxy("#main", {
+    scrollTop(value) {
+      return arguments.length
+        ? locoScroll.scrollTo(value, 0, 0)
+        : locoScroll.scroll.instance.scroll.y;
+    }, // we don't have to define a scrollLeft because we're only scrolling vertically.
+    getBoundingClientRect() {
+      return {
+        top: 0,
+        left: 0,
+        width: window.innerWidth,
+        height: window.innerHeight,
+      };
+    },
+    // LocomotiveScroll handles things completely differently on mobile devices - it doesn't even transform the container at all! So to get the correct behavior and avoid jitters, we should pin things with position: fixed on mobile. We sense it by checking to see if there's a transform applied to the container (the LocomotiveScroll-controlled element).
+    pinType: document.querySelector("#main").style.transform
+      ? "transform"
+      : "fixed",
+  });
+
+  // each time the window updates, we should refresh ScrollTrigger and then update LocomotiveScroll.
+  ScrollTrigger.addEventListener("refresh", () => locoScroll.update());
+
+  // after everything is set up, refresh() ScrollTrigger and update LocomotiveScroll because padding may have been added for pinning, etc.
+  ScrollTrigger.refresh();
+}
+
+locomotive();
+
 const tl = gsap.timeline();
 const crs = document.querySelector("#cursor");
 const crsBlur = document.querySelector("#cursor-blur");
@@ -29,7 +71,7 @@ tl.to("#nav", {
   duration: 1,
   scrollTrigger: {
     trigger: "#nav",
-    scroller: "body",
+    scroller: "#main",
     start: "top -10%",
     end: "top -11%",
     scrub: 2,
@@ -40,7 +82,7 @@ tl.to("#main", {
   backgroundColor: "#000",
   scrollTrigger: {
     trigger: "#main",
-    scroller: "body",
+    scroller: "#main",
     start: "top -30%",
     end: "top -80%",
     scrub: 2,
@@ -53,7 +95,7 @@ tl.from("#about-us img, #about", {
   duration: 1,
   scrollTrigger: {
     trigger: "#about-us",
-    scroller: "body",
+    scroller: "#main",
     start: "top 70%",
     end: "top 30%",
     scrub: 2,
@@ -66,7 +108,7 @@ tl.from(".card", {
   duration: 1,
   scrollTrigger: {
     trigger: ".card",
-    scroller: "body",
+    scroller: "#main",
     start: "top 80%",
     end: "top 50%",
     scrub: 1,
@@ -81,7 +123,7 @@ gsap.from("#colon1", {
   duration: 1,
   scrollTrigger: {
     trigger: "#colon1",
-    scroller: "body",
+    scroller: "#main",
     // start: "top 80%",
     // end: "top 50%",
     // scrub: 1,
@@ -94,7 +136,7 @@ gsap.from("#colon2", {
   duration: 1,
   scrollTrigger: {
     trigger: "#colon2",
-    scroller: "body",
+    scroller: "#main",
     // markers: true,
     // start: "top 80%",
     end: "top 80%",
@@ -107,7 +149,7 @@ gsap.from("#page4 h4", {
   duration: 1,
   scrollTrigger: {
     trigger: "#page4",
-    scroller: "body",
+    scroller: "#main",
     // markers: true,
     // start: "top 80%",
     end: "top 80%",
